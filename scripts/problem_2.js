a0a7c0a6883c005f8a97bd91157da24bd4a7296b
swarupSikder <swarupfx20@gmail.com>
2025-06-13 20:40:33 +0600

    first commit

---
+++ scripts/problem_2.js
@@ -0,0 +1,10 @@
+const checkEvenOdd = (num) => {
+    if(num%2 === 0){
+        console.log(num + " is even");
+    }
+    else{
+        console.log(num + " is odd");
+    }
+}
+
+checkEvenOdd(12);
